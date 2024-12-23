c938e09112726a816374f2515639affedf0f17cb
nishi-oka <nishioka31415@gmail.com>
2024-12-24 07:11:03 +0900

    changed background color

--- src/App.js
+++ src/App.js
@@ -9,16 +9,14 @@ import Decoration from "./Decoration";
 function App() {
   const [randomNumber, setRandomNumber] = useState(null); // 現在の数字
   const [pastNumbers, setPastNumbers] = useState([]); // 過去の数字
-  const [isDisabled, setIsDisabled] = useState(false); // Startボタンの無効化
+  const [isDisabled, setIsDisabled] = useState(false); // ボタンの無効化
   const [fadeOut, setFadeOut] = useState(false); // フェードアウト状態
-  const [isGenerating, setIsGenerating] = useState(false); // generateRandomNumber 処理中かどうか
 
   // 新しい数字を生成する関数
   const generateRandomNumber = () => {
-    if (isDisabled || isGenerating) return; // ボタンが無効化されているか、処理中なら何もしない
-    setIsGenerating(true); // 処理中フラグを立てる
-    setIsDisabled(true); // Startボタンを無効化
+    if (isDisabled) return; // ボタンが無効化されている場合は何もしない
 
+    setIsDisabled(true); // ボタンを無効化
     setFadeOut(true);
 
     setTimeout(() => {
@@ -37,18 +35,27 @@ function App() {
       });
 
       setFadeOut(false); // フェードアウトを終了
-      setIsDisabled(false); // Startボタンを有効化
-      setIsGenerating(false); // 処理終了フラグを解除
-    }, 1000); // フェードアウトアニメーションの時間を調整
+    }, 900); // フェードアウトアニメーションの時間を調整
+
+    // 2秒後にボタンを再度有効化
+    setTimeout(() => {
+      setIsDisabled(false); // ボタンを有効化
+    }, 3000);
   };
 
   // リセット関数: 初期状態に戻す
   const resetState = () => {
-    if (isGenerating) return; // generateRandomNumber 処理中なら何もしない
+    if (isDisabled) return; // ボタンが無効化されている場合は何もしない
+
     setRandomNumber(null);
     setPastNumbers([]);
-    setIsDisabled(false);
     setFadeOut(false);
+    setIsDisabled(true); // リセット時も2秒間無効化
+
+    // 2秒後にボタンを再度有効化
+    setTimeout(() => {
+      setIsDisabled(false); // ボタンを有効化
+    }, 2000);
   };
 
   return (
@@ -56,8 +63,8 @@ function App() {
       <div className="content">
         <Decoration />
         <CurrentNumber number={randomNumber} fadeOut={fadeOut} />
-        <StartButton onClick={generateRandomNumber} isDisabled={isDisabled} />
-        <ResetButton onClick={resetState} isDisabled={isGenerating} /> {/* Resetボタンを無効化 */}
+        <StartButton onClick={generateRandomNumber} isDisabled={isDisabled} /> {/* ボタン無効化 */}
+        <ResetButton onClick={resetState} isDisabled={isDisabled} /> {/* Resetボタンも無効化 */}
       </div>
       <PreNumber numbers={pastNumbers} />
     </div>
